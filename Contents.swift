14f65601230c1b43da2aeb6f600064e330f1f7c8
Nattapat <san.1234@hotmail.com>
2017-12-10 11:51:39 +0700

    test

---
+++ Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Explicit Variable to String Datatype
+var numberString: String = "4"
+
+//Chang Data Type String ==> Integer
+var numberInt = Int(numberString)
+//var answerInt = numberInt! + 10
+
+//Solution for nil
+if let myNumberInt = numberInt {
+    var answerInt = myNumberInt + 10
+} //if
+
